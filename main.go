5b453468542f92c52343b085740fe9af5b63dac7
Boris Ryabov <stonks33@gmail.com>
2022-04-19 23:10:32 -0700

    [ADD, FIX, MAKE]: Initial Project Backend Structure  and adding .mod to project.
    [ADD, FIX, MAKE]: Initial Project Backend Structure
    and adding .mod to project.
    
    Problem
    We have an initial project structure, but the go lang doesn't compile.
    
    Solution
    Created a .mod file and edited main.go file.
    
    Notes:
    The template will be posted on notion.

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"ambassador/src/database"
-	"ambassador/src/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
-	database.Connect()
-	database.AutoMigrate()
-
 	app := fiber.New()
 
-	app.Use(cors.New(cors.Config{
-		AllowCredentials: true,
-	}))
-
-	routes.Setup(app)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World desu~~~~~!!!!!!!")
+	})
 
 	app.Listen(":8000")
 }
