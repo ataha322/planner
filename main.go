814aa53bb5762ed4c06ece5a6bb2fb5f12048bf4
Boris Ryabov <stonks33@gmail.com>
2022-04-27 00:59:57 -0700

    [ADD, FIX, REFACTOR]: creating database, fixing docker-compose.yaml for proper SQL connection.
    [ADD, FIX, REFACTOR]: creating database, fixing docker-compose.yaml for proper SQL connection.
    
    Problem
    1. The app didn't have a database
    
    2. current `docker-compose.yaml` config didn't work with M1 apple chips, throwing errors when running.
    
    Solution
    1. Refactor `docker-compose.yaml` by specifing  MYSQL_HOST and upping docker version.
    
    2. Adding more MySQL configs to `docker-compose.yaml`
    
    3. Creating app's database by using `grom.Open()` function.
    
    Note
    Needs testing.

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/plannerData"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could no find the database")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
