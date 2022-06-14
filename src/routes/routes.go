33ce9e7f0696f01eba2dcb7abe9c28a0ded083cd
Boris Ryabov <stonks33@gmail.com>
2022-06-14 10:00:43 -0700

    [ADD] Add basic routing
    [ADD] Add basic routing
    
    Problem:
    We made controllers, not routes
    
    Solution:
    Made added basic routing
    
    Note:
    We have to disscuss the structure of task

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"planner.xyi/src/controllers"
+	"planner.xyi/src/middlewares"
 )
 
 func Setup(app *fiber.App) {
@@ -10,4 +11,15 @@ func Setup(app *fiber.App) {
 	admin := api.Group("admin")
 
 	admin.Post("register", controllers.Register)
+	admin.Post("login", controllers.Login)
+
+	adminAuthenticated := admin.Use(middlewares.IsAuthenticated)
+	adminAuthenticated.Get("user", controllers.User)
+	adminAuthenticated.Post("logout", controllers.Logout)
+	adminAuthenticated.Put("user/info", controllers.UpdateInfo)
+	adminAuthenticated.Put("user/password", controllers.UpdatePassword)
+
+	// ? I supouse we wouldn't have tasks that are not attached of a user
+	adminAuthenticated.Get("user/task")
+
 }
